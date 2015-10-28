93ddc4c8c2413ea3d491da59d9a7c4008d8f8601
张杰 <zhangjie@duoquyuedu.com>
2015-10-28 16:45:20 +0800

    添加图片拉伸效果

--- git指令/ViewController.m
+++ git指令/ViewController.m
@@ -8,20 +8,69 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+{
+    UIView *_headView;
 
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UITableView *tableview = [[UITableView alloc]init];
+    tableview.frame = self.view.frame;
+    tableview.delegate = self;
+    tableview.dataSource = self;
+    [self.view addSubview:tableview];
+    
+    UIView *headView = [[UIView alloc]init];
+    headView.frame = CGRectMake(0, 0, self.view.bounds.size.width, 200);
+    headView.backgroundColor = [UIColor yellowColor];
+    _headView = headView;
+    
+    UIView *headView1 = [[UIView alloc]init];
+    headView1.frame = CGRectMake(0, 0, self.view.bounds.size.width, 200);
+    headView1.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:headView];
+    tableview.tableHeaderView = headView1;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc]init];
+    cell.textLabel.text = @"sfdsfds";
+    return cell;
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    
+    CGFloat offset = scrollView.contentOffset.y;
+    if (offset<0) {
+        
+        CGFloat w = (ABS(offset) + 200)/200 * self.view.bounds.size.width;
+        CGFloat x = -(w - self.view.bounds.size.width)/2.0;
+        _headView.frame = CGRectMake(x, 0, w, ABS(offset) + 200);
+        
+        
+    }else{
+    
+        _headView.frame = CGRectMake(0, 0, self.view.bounds.size.width, 200-ABS(offset));
+    
+    }
+
 }
 
 @end
